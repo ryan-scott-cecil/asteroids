75810495df1a9fbe56a8fbd78682557592287698
DangerWill <60117469+DangerWill@users.noreply.github.com>
2024-10-11 07:47:07 -0700

    got the screen painted black, the programming is closing as expected, and .gitignore has been created.

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        screen.fill(
-            
-        )
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
 
 
 if __name__ == "__main__":

0b6f45d0c85a8c9c069634d5631178751f472e14
DangerWill <60117469+DangerWill@users.noreply.github.com>
2024-10-25 08:38:23 -0700

    added weapon cooldown timer to player class

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -22,6 +23,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.cooldown -= dt
 
         if keys[pygame.K_a]:
             # make it turn counter clockwise
@@ -41,8 +43,11 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
+        if self.cooldown > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.cooldown = PLAYER_SHOOT_COOLDOWN
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
